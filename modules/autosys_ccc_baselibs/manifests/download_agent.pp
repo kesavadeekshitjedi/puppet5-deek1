fc980e19c1fc190c21f6acfd92f3ea8803075ba7
Deekshit Addepalli <kesavadeekshit@yahoo.com>
2018-04-03 15:41:28 -0500

    Fixed unzip issues in download_agent.pp

--- modules/autosys_ccc_baselibs/manifests/download_agent.pp
+++ modules/autosys_ccc_baselibs/manifests/download_agent.pp
@@ -3,7 +3,7 @@ class autosys_ccc_baselibs::download_agent {
   $fileserver_agent_media_loc = "agent_media"
   $agent_media_targz_name="linux_agent_114_x86.tar.Z"
   $agent_media_tar_name="linux_agent_114_x86.tar"
-  $fileserverbase_dwnld_loc = "http://$fileserverhostname/$fileserver_agent_media_loc/$agent_media_tar_name"
+  $fileserverbase_dwnld_loc = "http://$fileserverhostname/$fileserver_agent_media_loc/$agent_media_targz_name"
   $download_directory = "/opt"
   $agent_unzip_directory = "/opt/agent_installer"
 

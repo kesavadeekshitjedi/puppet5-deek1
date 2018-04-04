4d4fa51e717f0a408ba836b5259ea387f5ae26e2
Deekshit Addepalli <kesavadeekshit@yahoo.com>
2018-04-04 09:44:25 -0500

    back to basics to debug

--- modules/autosys_ccc_baselibs/manifests/download_agent.pp
+++ modules/autosys_ccc_baselibs/manifests/download_agent.pp
@@ -3,9 +3,17 @@ class autosys_ccc_baselibs::download_agent {
   $fileserver_agent_media_loc = "agent_media"
   $agent_media_targz_name="linux_agent_114_x86.tar.Z"
   $agent_media_tar_name="linux_agent_114_x86.tar"
-  $fileserverbase_dwnld_loc = "http://$fileserverhostname/$fileserver_agent_media_loc/$agent_media_targz_name"
+  $fileserverbase_dwnld_loc = "http://${fileserverhostname}/${fileserver_agent_media_loc}/${agent_media_targz_name}"
   $agent_unzip_directory = "/opt/agent_installer/"
 
+  file
+  {
+    $agent_unzip_directory:
+    ensure => directory,
+    owner => 'root',
+    group => 'root',
+    mode => '0755'
+  }
 
 
 }

79acb707534f737722d2242d322c6730081f6c07
Deekshit Addepalli <kesavadeekshit@yahoo.com>
2018-04-04 13:18:39 -0500

    fixed missing comma in setPermissions block

--- modules/autosys_ccc_baselibs/manifests/install_agent.pp
+++ modules/autosys_ccc_baselibs/manifests/install_agent.pp
@@ -18,7 +18,7 @@ cwd => $agent_unzip_directory
 exec {
   'setPermissions':
   command => "chmod -R 755 $agent_unzip_directory"
-  user => 'root'
+  user => 'root',
   require => Exec['untarFile']
 }
   file

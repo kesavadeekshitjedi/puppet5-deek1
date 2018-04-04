c8d8a1137644e58d3276307188bd7442871109f6
Deekshit Addepalli <kesavadeekshit@yahoo.com>
2018-04-04 11:14:23 -0500

    wget is working in download_agent.pp. Testing gunzip in download_agent.pp

--- modules/autosys_ccc_baselibs/manifests/download_agent.pp
+++ modules/autosys_ccc_baselibs/manifests/download_agent.pp
@@ -1,11 +1,25 @@
 class autosys_ccc_baselibs::download_agent {
-  $fileserverhostname="rh7-sq1"
-  $fileserver_agent_media_loc = "agent_media"
+  $fileserverhostname="http://rh7-sq1/"
+  $fileserver_agent_media_loc = "agent_media/"
   $agent_media_targz_name="linux_agent_114_x86.tar.Z"
   $agent_media_tar_name="linux_agent_114_x86.tar"
   $fileserverbase_dwnld_loc = "http://${fileserverhostname}/${fileserver_agent_media_loc}/${agent_media_targz_name}"
   $agent_unzip_directory = "/opt/agent_installer/"
 
+define get_remote_file($remote_file=undef,$mode='0755')
+{
+  exec{'retrieve_${title}':
+  command => "wget -v ${fileserverhostname}/${fileserver_agent_media_loc}/${remote_file} -O ${title}",
+  path => ['/usr/bin'],
+  creates => $title,
+      }
+
+  file {${title}:
+        mode => $mode,
+        require => Exec['retrieve_${title}'],
+       }
+
+}
   file
   {
     $agent_unzip_directory:
@@ -15,23 +29,19 @@ class autosys_ccc_baselibs::download_agent {
     mode => '0755'
   }
 ->
-  exec
-  {
-    'getAgentZ':
-    command => "wget http://${fileserverhostname}/agent_media/$agent_media_targz_name",
-    path => ['/usr/bin','/usr/sbin'],
-    cwd => $agent_unzip_directory
 
-  }
-->
-  exec {
-    'deflate_gz':
-    command => 'gzip -dc ${agent_media_targz_name} | tar xvf -',
-    path => ['/usr/bin','/usr/sbin','/bin'],
-    cwd => '/opt/agent_installer',
-    require => Exec['getAgentZ']
+exec {'getAgentZ':
 
-  }
+command => "/usr/bin/wget -v ${fileserverhostname}${fileserver_agent_media_loc}${agent_media_targz_name} -O /opt/agent_installer/linux_agent_114_x86.tar.Z"
+owner => 'root',
+require => File[$agent_unzip_directory],
+}
+
+exec{'deflate_gz':
+command => "/usr/bin/gunzip /opt/agent_installer/linux_agent_114_x86.tar.Z",
+owner => "root",
+require => Exec['getAgentZ'],
+}
 
 
 

69ccab431653f9464c54b543f2fb3737b45d8794
Deekshit Addepalli <kesavadeekshit@yahoo.com>
2018-04-05 15:52:19 -0500

    Added logic to install agent only if it is not already installed

--- modules/autosys_ccc_baselibs/manifests/install_agent.pp
+++ modules/autosys_ccc_baselibs/manifests/install_agent.pp
@@ -8,87 +8,91 @@ class autosys_ccc_baselibs::install_agent
   $agent_unzip_directory = "/opt/agent_installer/"
 
 
-
-
-  file
+  if( $::ae_agent_install_version ==undef or $::ae_agent_install_version == nil)
   {
-    $agent_unzip_directory:
+    notice('AutoSys agent not installed on server. Starting installation...')
+
+
+    file
+    {
+      $agent_unzip_directory:
+      ensure => directory,
+      owner => 'root',
+      group => 'root',
+      mode => '0755'
+    }
+  ->
+
+  exec
+  {
+    'getAgenttar':
+    command => "wget -v ${fileserverhostname}/agent_media/$agent_media_tar_name",
+    path => ['/usr/bin','/usr/sbin'],
+    cwd => $agent_unzip_directory,
+    timeout => 0
+
+
+  }
+  exec
+  {
+    'getMD5':
+    command => "wget -v ${fileserverhostname}/agent_media/agent.md5",
+    path => ['/usr/bin','/usr/sbin'],
+    cwd => $agent_unzip_directory,
+    require => Exec['getAgenttar']
+  }
+
+  exec
+  {
+    'getLocalMD5':
+    command => "md5sum ${agent_media_tar_name} > localagent.md5",
+    path => ['/usr/bin','/usr/sbin'],
+    cwd => $agent_unzip_directory,
+    require => Exec['getMD5']
+  }
+
+  exec {
+    'localMD5':
+    command => 'cat localagent.md5 | awk \'{print $1}\' > local.md5',
+    path => ['/usr/bin','/usr/sbin'],
+    cwd => $agent_unzip_directory,
+    require => Exec['getLocalMD5']
+  }
+
+  exec {
+    'untarFile':
+    command => 'tar -xvf linux_agent_114_x86.tar',
+    path => ['/usr/bin','/usr/sbin'],
+    cwd => $agent_unzip_directory,
+    require => Exec['getLocalMD5','getAgenttar']
+  }
+
+  ->
+
+    file {'checkAgentFile':
+    path => "/opt/agent_installer/linux_x86_64",
     ensure => directory,
     owner => 'root',
     group => 'root',
-    mode => '0755'
-  }
-->
-
-exec
-{
-  'getAgenttar':
-  command => "wget -v ${fileserverhostname}/agent_media/$agent_media_tar_name",
-  path => ['/usr/bin','/usr/sbin'],
-  cwd => $agent_unzip_directory,
-  timeout => 0
-
-
-}
-exec
-{
-  'getMD5':
-  command => "wget -v ${fileserverhostname}/agent_media/agent.md5",
-  path => ['/usr/bin','/usr/sbin'],
-  cwd => $agent_unzip_directory,
-  require => Exec['getAgenttar']
-}
-
-exec
-{
-  'getLocalMD5':
-  command => "md5sum ${agent_media_tar_name} > localagent.md5",
-  path => ['/usr/bin','/usr/sbin'],
-  cwd => $agent_unzip_directory,
-  require => Exec['getMD5']
-}
-
-exec {
-  'localMD5':
-  command => 'cat localagent.md5 | awk \'{print $1}\' > local.md5',
-  path => ['/usr/bin','/usr/sbin'],
-  cwd => $agent_unzip_directory,
-  require => Exec['getLocalMD5']
-}
-
-exec {
-  'untarFile':
-  command => 'tar -xvf linux_agent_114_x86.tar',
-  path => ['/usr/bin','/usr/sbin'],
-  cwd => $agent_unzip_directory,
-  require => Exec['getLocalMD5','getAgenttar']
-}
-
-->
-
-  file {'checkAgentFile':
-  path => "/opt/agent_installer/linux_x86_64",
-  ensure => directory,
-  owner => 'root',
-  group => 'root',
-  mode => '0755',
-  require=> Exec['untarFile']
-  }
-  exec {
-    'setPermissions':
-    command => "chmod -R 755 $agent_unzip_directory",
-    path => ['/usr/bin','/usr/sbin'],
+    mode => '0755',
+    require=> Exec['untarFile']
+    }
+    exec {
+      'setPermissions':
+      command => "chmod -R 755 $agent_unzip_directory",
+      path => ['/usr/bin','/usr/sbin'],
+      user => 'root',
+      require => Exec['untarFile']
+    }
+    ->
+    exec {'agentInstall':
+    command => "/opt/agent_installer/linux_x86_64/setup.bin -f /opt/agent_installer/unix_installer.properties -i silent",
     user => 'root',
-    require => Exec['untarFile']
-  }
-  ->
-  exec {'agentInstall':
-  command => "/opt/agent_installer/linux_x86_64/setup.bin -f /opt/agent_installer/unix_installer.properties -i silent",
-  user => 'root',
-  returns => 0,
-  timeout => 0,
-  require => Exec['untarFile','getAgenttar']
-  }
+    returns => 0,
+    timeout => 0,
+    require => Exec['untarFile','getAgenttar']
+    }
 
+  }
 
 }

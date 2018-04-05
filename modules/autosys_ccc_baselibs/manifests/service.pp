b40545d1377bd1b78faa2a3699d058f599ce2115
Deekshit Addepalli <kesavadeekshit@yahoo.com>
2018-04-05 11:47:58 -0500

    Added the service portion. Testing to see if it works

--- modules/autosys_ccc_baselibs/manifests/service.pp
+++ modules/autosys_ccc_baselibs/manifests/service.pp
@@ -1,3 +1,24 @@
 class autosys_ccc_baselibs::service{
-  
+
+  file {'/etc/init.d/waae_agent-WA_AGENT':
+  ensure => present,
+  mode => '0755',
+  owner => 'root',
+  group => 'root',
+  source => "puppet://${servername}/modules/${module_name}/waae_agent-WA_AGENT"
+        }
+
+  file {'/opt/CA/WorkloadAutomationAE/SystemAgent/WA_AGENT/cryptkey.txt':
+  ensure => present,
+  owner => 'root',
+  group => 'root',
+  mode => '0755',
+  source => "puppet://${servername}/modules/${module_name}/cryptkey.txt"
+  }
+
+  service {'waae_agent-WA_AGENT' :
+  ensure => running,
+  user => 'root',
+  name => 'waae_agent-WA_AGENT'
+  }
 }

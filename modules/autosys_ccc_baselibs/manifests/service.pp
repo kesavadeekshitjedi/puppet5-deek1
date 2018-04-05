bf9a48e500d8c07dd5197fe00a3c0f9b86b5aaeb
Deekshit Addepalli <kesavadeekshit@yahoo.com>
2018-04-05 11:59:06 -0500

    Added dependency to the service class

--- modules/autosys_ccc_baselibs/manifests/service.pp
+++ modules/autosys_ccc_baselibs/manifests/service.pp
@@ -5,20 +5,22 @@ class autosys_ccc_baselibs::service{
   mode => '0755',
   owner => 'root',
   group => 'root',
-  source => "puppet://${servername}/modules/${module_name}/waae_agent-WA_AGENT"
-        }
+  source => "puppet://${servername}/modules/${module_name}/waae_agent-WA_AGENT",
+  require => [Class["autosys_ccc_baselibs::install_agent"]]
+  }
 
   file {'/opt/CA/WorkloadAutomationAE/SystemAgent/WA_AGENT/cryptkey.txt':
   ensure => present,
   owner => 'autosys',
   group => 'autosys',
-  mode => '0755',
+  mode => '0644',
   source => "puppet://${servername}/modules/${module_name}/cryptkey.txt"
   }
 
   service {'waae_agent-WA_AGENT' :
   ensure => running,
   enable => 'true',
-  name => 'waae_agent-WA_AGENT'
+  name => 'waae_agent-WA_AGENT',
+  reqire => File['/etc/init.d/waae_agent-WA_AGENT']
   }
 }
